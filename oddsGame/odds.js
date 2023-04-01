8d99561434669265dcddf598a09c62be4166cedf
Matthew <matt-bagnato@hotmail.com>
2023-04-01 16:58:36 +1100

    finished odds game

--- oddsGame/odds.js
+++ oddsGame/odds.js
@@ -2,18 +2,20 @@ const play = document.querySelector("#start-but");
 const time = document.querySelector("#countDown");
 let count = 3;
 
+
 const playAudio = function () {
+    document.querySelector("#start-but").disabled = true;
+
     let audio = new Audio('audio/countdown.mp3')
     audio.play();
 
         function countdown() {
             time.textContent = count;
             count--;
-
+            
             if(count === -1) {
                 time.textContent = "GO"
                 clearInterval(interval);
-                document.querySelector("#start-but").disabled = true;
             } 
         }
         const interval = setInterval(countdown, 1500);
@@ -23,6 +25,7 @@ play.addEventListener("click", playAudio);
 const reset = document.querySelector("#restart");
 
 reset.addEventListener("click", function() {
+    time.textContent = 3;
     document.querySelector("#start-but").disabled = false;
     count = 3;
     play.addEventListener("click", playAudio);
